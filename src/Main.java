c13fbdeecf2f4ad3ed4316cbf7b2bc1987d60c54
Aituar03 <arpabaev_aituar@mail.ru>
2022-11-12 19:33:08 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Scanner scan=new Scanner(System.in);
+        String s=scan.next();
+    }
+}
